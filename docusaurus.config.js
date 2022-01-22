ee9fd6e81f13e2967e4f2c4761116bdb00e4b5d2
Andrei Horbach <a.horbach@studionx.ru>
2022-01-22 21:27:00 +0300

    chore: resolve links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -74,7 +74,7 @@ const config = {
             items: [
               {
                 label: 'Demo',
-                to: '/docs/intro',
+                to: '/docs/demo',
               },
             ],
           },
